79f043631bfa483bd20a95620c71971fdc405c4a
kittech <justfoxxo@Outlook.com>
2023-12-01 20:23:35 +0100

    clean

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@ use crate::frontend::start_app;
 const APP_ID: &str = "io.github.justfoxx.guicachefs";
 
 fn main() -> Result<(), String> {
-    let exit_code = start_app();
-    match exit_code {
+    match start_app() {
         ExitCode::SUCCESS => Ok(()),
         ExitCode::FAILURE => Err("Failed to start application".to_string()),
         x => Err(format!("Unknown exit code: {}",x.value())),
