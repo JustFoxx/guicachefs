71c45439c32d9fc860ebca8de53cc6d7915f3b8f
kittech <justfoxxo@Outlook.com>
2023-12-01 19:42:49 +0100

    edit the text

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use std::rc::Rc;
 use std::sync::atomic::{AtomicU32, Ordering};
-use gtk4::{Application, ApplicationWindow, Button, glib};
-use gtk4::glib::PropertyGet;
-use gtk4::prelude::{ApplicationExt, ApplicationExtManual, ButtonExt};
+use gtk4::{Align, Application, ApplicationWindow, Button, glib, Text};
+use gtk4::{Grid};
+use gtk4::prelude::{ApplicationExt, ApplicationExtManual, ButtonExt, EditableExt, GridExt, WidgetExt};
 use gtk4::traits::GtkWindowExt;
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld1";
@@ -12,32 +13,47 @@ fn main() -> glib::ExitCode {
         .application_id(APP_ID)
         .build();
 
-    app.connect_activate(app_main);
+    app.connect_activate(build_ui);
     app.run()
 }
 
-fn app_main(app: &Application) {
+fn build_ui(app: &Application) {
+    let grid = &Grid::builder()
+        .halign(Align::Center)
+        .valign(Align::Center)
+        .build();
+
     let window = ApplicationWindow::builder()
         .application(app)
         .default_width(320)
         .default_height(200)
         .resizable(false)
         .title("Guicachefs")
+        .child(grid)
         .build();
 
-    let button = Button::builder()
-        .label("Click me!")
-        .margin_bottom(50)
-        .margin_top(50)
-        .margin_start(100)
-        .margin_end(100)
-        .build();
-    static I: AtomicU32 = AtomicU32::new(1);
-    button.connect_clicked(|btn| {
-        let i = I.fetch_add(1, Ordering::SeqCst);
+    let components = Rc::new((
+        Button::builder()
+            .label("Click me!")
+            .build(),
+
+        Text::builder()
+            .editable(true)
+            .text("cool")
+            .build(),
+    ));
+
+    let i = AtomicU32::new(1);
+    let copy_text = components.1.clone();
+
+    components.0.connect_clicked(move |btn| {
+        let i = i.fetch_add(1, Ordering::SeqCst);
         btn.set_label(&format!("{i} clicks!"));
+        println!("{}", copy_text.text())
     });
-    window.set_child(Some(&button));
-    // Show the window.
+
+    grid.attach(&components.0, 0, 0, 10, 1);
+    grid.attach_next_to(&components.1, Some(&components.0), gtk4::PositionType::Right, 1, 1);
+
     window.present();
 }
