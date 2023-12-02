68e159de4b0c3aba65691aed043ac7189bd21717
kittech <justfoxxo@Outlook.com>
2023-12-02 10:56:50 +0100

    clean up

--- src/frontend.rs
+++ src/frontend.rs
@@ -10,12 +10,21 @@ pub fn start_app() -> ExitCode {
         .application_id(APP_ID)
         .build();
 
-    app.connect_activate(build_window);
+    app.connect_activate(build_app);
     app.run()
 }
 
-fn build_window(app: &Application) {
-    let grid = &Grid::builder()
+fn build_app(app: &Application) {
+    let grid = build_grid();
+    let window = build_window(app, &grid);
+    let components = Rc::new(build_components());
+    build_callback(&components);
+    attach_components(&grid, &components);
+    window.present();
+}
+
+fn build_grid() -> Grid {
+    Grid::builder()
         .halign(Align::Center)
         .column_spacing(10)
         .row_spacing(10)
@@ -23,22 +32,20 @@ fn build_window(app: &Application) {
         .margin_top(10)
         .margin_start(10)
         .margin_end(10)
-        .build();
+        .build()
+}
 
-    let window = &ApplicationWindow::builder()
+fn build_window(app: &Application, grid: &Grid) -> ApplicationWindow {
+    ApplicationWindow::builder()
         .application(app)
         .resizable(false)
         .title("Guicachefs")
         .child(grid)
-        .build();
-
-    build_ui(grid);
-
-    window.present();
+        .build()
 }
 
-fn build_ui(grid: &Grid) {
-    let components = Rc::new((
+fn build_components() -> (Button, Text, Text, Text, Text) {
+    (
         Button::builder()
             .label("Click me!")
             .build(),
@@ -57,8 +64,18 @@ fn build_ui(grid: &Grid) {
         Text::builder()
             .editable(false)
             .build(),
-    ));
+    )
+}
 
+fn attach_components(grid: &Grid, components: &Rc<(Button, Text, Text, Text, Text)>) {
+    grid.attach(&components.0, 0, 0, 1, 1);
+    grid.attach(&components.1, 1, 0, 1, 1);
+    grid.attach(&components.2, 0, 1, 1, 1);
+    grid.attach(&components.3, 1, 1, 1, 1);
+    grid.attach(&components.4, 2, 1, 1, 1);
+}
+
+fn build_callback(components: &Rc<(Button, Text, Text, Text, Text)>) {
     static I: AtomicU32 = AtomicU32::new(1);
     let string_text = components.1.clone();
     let (text0, text1, text2) = (components.2.clone(), components.3.clone(), components.4.clone());
@@ -81,10 +98,4 @@ fn build_ui(grid: &Grid) {
             text1.set_text("");
         }
     });
-
-    grid.attach(&components.0, 0, 0, 1, 1);
-    grid.attach(&components.1, 1, 0, 1, 1);
-    grid.attach(&components.2, 0, 1, 1, 1);
-    grid.attach(&components.3, 1, 1, 1, 1);
-    grid.attach(&components.4, 2, 1, 1, 1);
 }
