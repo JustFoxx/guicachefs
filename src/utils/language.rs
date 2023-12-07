205cc44d0b4154ac5c3de79adb551d7b2f8a4461
kittech <justfoxxo@outlook.com>
2023-12-07 17:48:13 +0100

    language.rs clean up

--- src/utils/language.rs
+++ src/utils/language.rs
@@ -6,18 +6,24 @@ static LANG_DIR: include_dir::Dir = include_dir!("langs");
 
 type LangMap = HashMap<String,String>;
 
-pub static LANG: Lazy<LangMap> = Lazy::new(|| {
-    let lang = sys_locale::get_locale().unwrap_or("en-US".to_string()).to_lowercase();
-    let file = LANG_DIR.get_file(format!("{lang}.json")).unwrap_or_else(|| {
-        println!("Language {lang} is yet not supported");
-        LANG_DIR.get_file("en-us.json").unwrap()
-    });
-    let contents = file.contents_utf8().unwrap();
-    serde_json::from_str(contents).unwrap()
-});
+pub static LANG: Lazy<LangMap> = Lazy::new(generate_language);
 
-pub static DEFAULT_LANG: Lazy<LangMap> = Lazy::new(|| {
-    let file = LANG_DIR.get_file("en-us.json").unwrap();
+pub static DEFAULT_LANG: Lazy<LangMap> = Lazy::new(generate_default_language);
+
+fn generate_default_language() -> LangMap {
+    let lang = "en-us";
+    let file = LANG_DIR.get_file(format!("{lang}.json")).unwrap();
     let contents = file.contents_utf8().unwrap();
     serde_json::from_str(contents).unwrap()
-});
+}
+
+fn generate_language() -> LangMap {
+    let lang = sys_locale::get_locale().unwrap_or("en-US".to_string()).to_lowercase();
+    let file = LANG_DIR.get_file(format!("{lang}.json"));
+    if file.is_none() {
+        println!("Language {lang} is yet not supported");
+        return generate_default_language();
+    }
+    let contents = file.unwrap().contents_utf8().unwrap();
+    serde_json::from_str(contents).unwrap()
+}
