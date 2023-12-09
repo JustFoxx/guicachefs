d135b8b2a09bfdb4aece79eab55da20efcda797d
kittech <justfoxxo@outlook.com>
2023-12-09 19:47:47 +0100

    Added actual options

--- src/app/components.rs
+++ src/app/components.rs
@@ -1,19 +1,67 @@
 use std::rc::Rc;
-use gtk4::{DropDown, Grid, Label};
+use gtk4::{DropDown, Grid, StringList};
 use gtk4::prelude::GridExt;
 
-pub type Components = (DropDown,Label);
+pub type Components = (DropDown);
+
+pub static OPTIONS: &[&str; 21] = &[
+    "",
+    "format",
+    "show-super",
+    "set-option",
+    "mount",
+    "fsck",
+    "fs usage",
+    "device",
+    "subvolume",
+    "data",
+    "unlock",
+    "set-passphrase",
+    "remove-passphrase",
+    "migrate",
+    "migrate-superblock",
+    "setattr",
+    "dump",
+    "list",
+    "list_journal",
+    "fusemount",
+    "version"
+];
+
+pub static DEVICES: &[&str; 9] = &[
+    "",
+    "add",
+    "remove",
+    "online",
+    "offline",
+    "evacuate",
+    "set-state",
+    "resize",
+    "resize-journal"
+];
+
+pub static SUBVOLUMES: &[&str; 4] = &[
+    "",
+    "create",
+    "delete",
+    "snapshot"
+];
+
+pub static DATAS: &[&str; 2] = &[
+    "replicate",
+    "job"
+];
 
 pub fn get_components() -> Components {
     (
-        DropDown::from_strings(&["Hello", "World"]),
-        Label::builder()
-            .label("0")
+        DropDown::builder()
+            .tooltip_text("Choose option")
+            .model(&StringList::new(OPTIONS))
+            .tooltip_text("Choose option. If the option is chosen, here will be description of it")
             .build()
     )
 }
 
 pub fn attach_components(grid: &Grid, components: &Rc<Components>) {
-    grid.attach(&components.0, 0, 0, 1, 1);
-    grid.attach(&components.1, 0, 1, 1, 1);
+    grid.attach(components.as_ref(), 0, 0, 1, 1);
 }
--- src/app/listeners.rs
+++ src/app/listeners.rs
@@ -1,15 +1,16 @@
 use std::rc::Rc;
-use gtk4::{DropDown, Label};
+use gtk4::{Button, DropDown, Label};
+use gtk4::prelude::WidgetExt;
 use crate::app::components::Components;
 
 pub fn subscribe(components: &Rc<Components>) {
-    components.0.connect_selected_item_notify(on_dropdown_selected_item_0(components.1.clone()));
+    components.connect_selected_item_notify(on_dropdown_selected_item_0());
 }
 
-fn on_dropdown_selected_item_0(text_component: Label) -> impl Fn(&DropDown) {
+fn on_dropdown_selected_item_0() -> impl Fn(&DropDown) {
     move |drop_down: &DropDown| {
         let selected = drop_down.selected();
-        text_component.set_text(&selected.to_string());
+        drop_down.set_tooltip_text(Some(&selected.to_string()));
         println!("{}", selected);
     }
 }
